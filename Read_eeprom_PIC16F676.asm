8feecaa63a52e658ed1d856093337e860a05fe02
Kondratyev-Sergey <56800110+Kondratyev-Sergey@users.noreply.github.com>
2025-02-15 16:42:27 +0200

    Add files via upload

---
+++ Read_eeprom_PIC16F676.asm
@@ -0,0 +1,24 @@
+;*******************************;*******************************
+; Read from eeprom-arguments: eeprom_address - address to eeprom, 
+; W - data from eeprom.
+;-------------------------------
+read_eeprom:
+;-------------------------------
+	MOVF 	eeprom_address,w;Write address
+;-------------------------------
+	BCF 	STATUS, RP0 	;
+	BSF 	STATUS, RP1 	;Bank 2
+;-------------------------------
+	MOVWF 	EEADR 		;to read from
+;-------------------------------
+	BSF 	STATUS, RP0 	;Bank 3
+	BCF 	EECON1, EEPGD 	;Point to Data memory
+	BSF 	EECON1, RD 	;Start read operation
+;-------------------------------
+	BCF 	STATUS, RP0 	;Bank 2
+	MOVF 	EEDATA, W 	;W = EEDATA
+;-------------------------------
+	BCF 	STATUS, RP0 	;
+	BCF 	STATUS, RP1 	;Bank 0
+;-------------------------------
+	return
