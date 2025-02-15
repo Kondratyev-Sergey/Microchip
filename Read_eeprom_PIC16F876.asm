613382b2baccad4fe3fd5c8406734a969c32273f
Kondratyev-Sergey <56800110+Kondratyev-Sergey@users.noreply.github.com>
2025-02-15 16:47:43 +0200

    Update Read_eeprom_PIC16F876.asm

--- Read_eeprom_PIC16F876.asm
+++ Read_eeprom_PIC16F876.asm
@@ -4,21 +4,21 @@
 ;-------------------------------
 read_eeprom:
 ;-------------------------------
-	MOVF 	eeprom_address,w;Write address
+	MOVF 	eeprom_address,w; Write address
 ;-------------------------------
 	BCF 	STATUS, RP0 	;
-	BSF 	STATUS, RP1 	;Bank 2
+	BSF 	STATUS, RP1 	; Bank 2
 ;-------------------------------
-	MOVWF 	EEADR 		;to read from
+	MOVWF 	EEADR 		; to read from
 ;-------------------------------
-	BSF 	STATUS, RP0 	;Bank 3
-	BCF 	EECON1, EEPGD 	;Point to Data memory
-	BSF 	EECON1, RD 	;Start read operation
+	BSF 	STATUS, RP0 	; Bank 3
+	BCF 	EECON1, EEPGD 	; Point to Data memory
+	BSF 	EECON1, RD 	; Start read operation
 ;-------------------------------
-	BCF 	STATUS, RP0 	;Bank 2
-	MOVF 	EEDATA, W 	;W = EEDATA
+	BCF 	STATUS, RP0 	; Bank 2
+	MOVF 	EEDATA, W 	; W = EEDATA
 ;-------------------------------
 	BCF 	STATUS, RP0 	;
-	BCF 	STATUS, RP1 	;Bank 0
+	BCF 	STATUS, RP1 	; Bank 0
 ;-------------------------------
 	return
